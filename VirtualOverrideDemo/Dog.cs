5a45ad4b73177c384532c7cd5adccbcbb0a84a15
Hazzard1912 <jhonnier.hernandez@correounivalle.edu.co>
2023-07-27 19:41:04 -0500

    Add project files.

---
+++ VirtualOverrideDemo/Dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    internal class Dog : Animal
+    {
+        public bool IsHappy { get; private set; }
+
+        public Dog(string name, int age) : base(name, age)
+        {
+            IsHappy = true;
+        }
+
+        // Sobreescribimos el metodo
+        public override void Eat()
+        {
+            // Llamado al metodo de la clase padre
+            base.Eat();
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!.");
+        }
+
+        public override void Play()
+        {
+            if (IsHappy)
+            {
+                base.Play();
+            }
+        }
+
+    }
+}
---
+++ VirtualOverrideDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualOverrideDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Sid", 15);
+
+            Console.WriteLine($"{dog.Name} is {dog.Age} years old");
+            dog.MakeSound();
+            dog.Play();
+            dog.Eat();
+        }
+    }
+}
